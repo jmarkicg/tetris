d204a266ebb40238829f6a2572cd97b38fe74fef
Jelena Markic Glavas <jele.markic@gmail.com>
2020-12-27 16:28:32 +0100

    Updated the draw matrix

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
-x = 0
 def animate_tetris_dot(x: number, y: number):
     led.plot(x, y)
     basic.pause(500)
     led.unplot(x, y)
 
+def draw_matrix(matrix: array):
+    for i in range(5):
+        for j in range(5):
+            if matrix[i][j] == 1:
+                led.plot(i, j)
+            else: 
+                led.unplot(i, j)
 def on_forever():
+    matrix = [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
     x = randint(0, 4)
     y = 0
     while y < 5:
+        draw_matrix(matrix)
         animate_tetris_dot(x, y)
-        y += 1
+        if matrix[x][y+1] == 0 and y != 4:
+            y += 1
+        else: 
+            matrix[x][y] = 1
+            break
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,15 +1,35 @@
-let x = 0
 function animate_tetris_dot(x: number, y: number) {
     led.plot(x, y)
     basic.pause(500)
     led.unplot(x, y)
 }
 
+function draw_matrix(matrix: number[][]) {
+    for (let i = 0; i < 5; i++) {
+        for (let j = 0; j < 5; j++) {
+            if (matrix[i][j] == 1) {
+                led.plot(i, j)
+            } else {
+                led.unplot(i, j)
+            }
+            
+        }
+    }
+}
+
 basic.forever(function on_forever() {
+    let matrix = [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
     let x = randint(0, 4)
     let y = 0
     while (y < 5) {
+        draw_matrix(matrix)
         animate_tetris_dot(x, y)
-        y += 1
+        if (matrix[x][y + 1] == 0 && y != 4) {
+            y += 1
+        } else {
+            matrix[x][y] = 1
+            break
+        }
+        
     }
 })
