0402dc5d0aeb15bb7644ad194ddade1d03aaffd9
Jelena Markic Glavas <jele.markic@gmail.com>
2020-12-27 16:41:16 +0100

    Implemented drawing of correct led cells of Tetris

--- main.py
+++ main.py
@@ -1,25 +1,29 @@
-def animate_tetris_dot(x: number, y: number):
-    led.plot(x, y)
-    basic.pause(500)
-    led.unplot(x, y)
-
-def draw_matrix(matrix: array):
+matrix = [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
+   
+def draw_matrix():
     for i in range(5):
         for j in range(5):
             if matrix[i][j] == 1:
                 led.plot(i, j)
             else: 
                 led.unplot(i, j)
+
+def animate_tetris_dot(x: number, y: number):
+    led.plot(x, y)
+    basic.pause(500)
+    led.unplot(x, y)
+
 def on_forever():
-    matrix = [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
     x = randint(0, 4)
     y = 0
     while y < 5:
-        draw_matrix(matrix)
+        draw_matrix()
         animate_tetris_dot(x, y)
         if matrix[x][y+1] == 0 and y != 4:
             y += 1
         else: 
             matrix[x][y] = 1
             break
+
+
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,10 +1,5 @@
-function animate_tetris_dot(x: number, y: number) {
-    led.plot(x, y)
-    basic.pause(500)
-    led.unplot(x, y)
-}
-
-function draw_matrix(matrix: number[][]) {
+let matrix = [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
+function draw_matrix() {
     for (let i = 0; i < 5; i++) {
         for (let j = 0; j < 5; j++) {
             if (matrix[i][j] == 1) {
@@ -17,12 +12,17 @@ function draw_matrix(matrix: number[][]) {
     }
 }
 
+function animate_tetris_dot(x: number, y: number) {
+    led.plot(x, y)
+    basic.pause(500)
+    led.unplot(x, y)
+}
+
 basic.forever(function on_forever() {
-    let matrix = [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
     let x = randint(0, 4)
     let y = 0
     while (y < 5) {
-        draw_matrix(matrix)
+        draw_matrix()
         animate_tetris_dot(x, y)
         if (matrix[x][y + 1] == 0 && y != 4) {
             y += 1
