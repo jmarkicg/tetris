18fd276933c3803ff309d5840916078b996e9ac1
Jelena Markic Glavas <jele.markic@gmail.com>
2020-12-27 17:19:00 +0100

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 matrix = [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
 end = 0
+y = 0
+x = 0
 
 def draw_matrix():
     for i in range(5):
@@ -9,24 +11,26 @@ def draw_matrix():
             else: 
                 led.unplot(i, j)
 
-def animate_tetris_dot(x: number, y: number):
+def animate_tetris_dot():
+    global x, y
     led.plot(x, y)
-    basic.pause(70)
+    basic.pause(700)
     led.unplot(x, y)
 
 def end_of_game():
     basic.show_icon(IconNames.SAD)
 
 def on_forever():
-    global end
-    x = randint(0, 4)
-    y = 0
+    global end, x, y
+
     if end == 1:
         end_of_game()
     else:
+        x = randint(0, 4)
+        y = 0
         while y < 5:
             draw_matrix()
-            animate_tetris_dot(x, y)
+            animate_tetris_dot()
             if matrix[x][y+1] == 0 and y != 4:
                 y = y + 1
             elif y == 0:
@@ -36,5 +40,18 @@ def on_forever():
                 matrix[x][y] = 1
                 break
 
+def on_button_pressed_a():
+    global x
+    x = x - 1
+    if x < 0:
+        x = 0
 
+def on_button_pressed_b():
+    global x
+    x = x + 1
+    if x > 4:
+        x = 4
+
+input.on_button_pressed(Button.A, on_button_pressed_a)
+input.on_button_pressed(Button.B, on_button_pressed_b)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
 let matrix = [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
 let end = 0
+let y = 0
+let x = 0
 function draw_matrix() {
     for (let i = 0; i < 5; i++) {
         for (let j = 0; j < 5; j++) {
@@ -13,9 +15,10 @@ function draw_matrix() {
     }
 }
 
-function animate_tetris_dot(x: number, y: number) {
+function animate_tetris_dot() {
+    
     led.plot(x, y)
-    basic.pause(70)
+    basic.pause(700)
     led.unplot(x, y)
 }
 
@@ -23,16 +26,32 @@ function end_of_game() {
     basic.showIcon(IconNames.Sad)
 }
 
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
+    x = x - 1
+    if (x < 0) {
+        x = 0
+    }
+    
+})
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    
+    x = x + 1
+    if (x > 4) {
+        x = 4
+    }
+    
+})
 basic.forever(function on_forever() {
     
-    let x = randint(0, 4)
-    let y = 0
     if (end == 1) {
         end_of_game()
     } else {
+        x = randint(0, 4)
+        y = 0
         while (y < 5) {
             draw_matrix()
-            animate_tetris_dot(x, y)
+            animate_tetris_dot()
             if (matrix[x][y + 1] == 0 && y != 4) {
                 y = y + 1
             } else if (y == 0) {
